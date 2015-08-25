a2a669f1c2eaaa930f24a91f27729dd5fdcf823f
conomia <conomia_alfredo@hotmail.com>
2015-08-25 17:56:17 -0500

    proyecto booking

---
+++ config.lua
@@ -0,0 +1,16 @@
+---------------------------------------------------------------------------------
+-- booking App
+-- Alfredo Chi
+-- GeekBucket Software Factory
+---------------------------------------------------------------------------------
+
+local mediaRes = display.pixelWidth  / 480
+
+application =
+{
+	content =
+	{
+		width = display.pixelWidth / mediaRes,
+        height = display.pixelHeight / mediaRes
+	},   
+}
