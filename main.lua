f7c9b3eb32b94448eb1c07a094c2674826fe9ad3
conomia <conomia_alfredo@hotmail.com>
2015-09-10 16:22:12 -0500

    mandar a login

--- main.lua
+++ main.lua
@@ -6,8 +6,17 @@
 
 -- Your code here
 
+local DBManager = require('src.resources.DBManager')
 local composer = require( "composer" )
 
 display.setStatusBar( display.DarkStatusBar )
 
-composer.gotoScene("src.Login")
+local isUser = DBManager.setupSquema()
+
+if not isUser then
+	composer.gotoScene( "src.Login" )
+else
+	composer.gotoScene( "src.Login" )
+	--composer.gotoScene("src.LoginGuard")
+	--composer.gotoScene("src.Home")
+end
